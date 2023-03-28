0129c484632d49966639f1d03c8f61d7faefa523
hathupham <111330275+hathupham@users.noreply.github.com>
2023-03-28 20:00:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Test AJAX</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-</head>
-<body>
-    <button id="btn-tinh-tong">Tính tổng</button>
-    <div id="result"></div>
-    <script>
-        $(document).ready(function() {
-            $("#btn-tinh-tong").click(function() {
-                $.ajax({
-                    url: "tinh_tong.php",
-                    dataType: "json",
-                    success: function(data) {
-                        $("#result").text("Kết quả là " + data.result);
-                    }
-                });
-            });
-        });
-    </script>
-</body>
-</html>
+<button id="generate-btn">Generate Password</button>
+
+<script>
+    document.getElementById('generate-btn').addEventListener('click', function() {
+        // Gọi API từ backend
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    // Phân tích kết quả trả về
+                    var data = JSON.parse(xhr.responseText);
+                    var password = data['password'];
+                    var message = data['message'];
+                    
+                    // Hiển thị kết quả
+                    document.getElementById('result').innerHTML = 'Password: ' + password + '<br>' +
+                                                                    'Message: ' + message;
+                } else {
+                    console.log('Error: ' + xhr.status);
+                }
+            }
+        };
+        xhr.open('GET', 'http://localhost/api/generate_password.php');
+        xhr.send();
+    });
+</script>
